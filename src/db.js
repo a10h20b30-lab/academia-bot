7c538e97433c56a9a0bd3911bf93fc569165bbe5
אברהם חי בנאי <avraham@MacBook-Air-sl-brhm.local>
2026-07-20 13:53:17 +0300

    debug database url

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 import pg from "pg";
 const { Pool } = pg;
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 30) + '...' : 'NOT SET');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
