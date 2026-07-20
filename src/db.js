bfc1a3c785f053e23acfb2b63a6944aaa3d9e2ab
אברהם חי בנאי <avraham@MacBook-Air-sl-brhm.local>
2026-07-20 13:11:28 +0300

    fix postgres SSL connection

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 export async function query(text, params) {
